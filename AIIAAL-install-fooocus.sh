63e80e05a8d978c4cee73e97fc55a991f804f54d
JT Gresham <119110518+JT-Gresham@users.noreply.github.com>
2024-05-30 06:18:39 -0400

    Update AIIAAL-install-fooocus.sh
    Added an /usr/bin command so fooocus can be started from the terminal directly.  Additionally, you can pass fooocus arguments directly with your new launcher.

--- AIIAAL-install-fooocus.sh
+++ AIIAAL-install-fooocus.sh
@@ -24,14 +24,16 @@ echo "Changing directory ->$pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc
 cd $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux
 echo ""
 echo "Cloning official $aiiaalpkg repository to $aiiaalpkg-IntelArc-ArchLinux"
-git clone "https://github.com/lllyasviel/Fooocus.git" "/tmp/$aiiaalpkg-IntelArc-ArchLinux"
+
+#### GIT CLONE COMMAND  URL HERE ####
+git clone "https://github.com/lllyasviel/$aiiaalpkg.git" "/tmp/$aiiaalpkg-IntelArc-ArchLinux"
 mv -f "/tmp/$aiiaalpkg-IntelArc-ArchLinux/.git" "$pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/"
 git checkout .
 rm -r /tmp/$aiiaalpkg-IntelArc-ArchLinux
 source $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/libref-$aiiaalpkg
 echo ""
-echo "Applying AIIAAL modifications to original fooocus..."
-AIIAAL_update_fooocus
+echo "Applying AIIAAL modifications to original $aiiaalpkg..."
+AIIAAL_update_$aiiaalpkg
 echo ""
 echo "Installing packages from requirements_versions.txt..."
 pip install -r requirements_versions.txt
@@ -41,15 +43,17 @@ touch $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg
 echo "#!/usr/bin/env bash" > $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 echo "" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 echo "source $pdirectory/AI-IntelArc-ArchLinux/libref" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
-echo "source /home/LACII14/Archive-M1/AI/AI-IntelArc-ArchLinux/fooocus-IntelArc-ArchLinux/libref-fooocus" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
+echo "source /home/LACII14/Archive-M1/AI/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/libref-$aiiaalpkg" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 echo "source $pdirectory/AI-IntelArc-ArchLinux/AI-IntelArc-ArchLinux_env/bin/activate" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 echo "AIIAAL_update" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 #### Executable below
-echo "AIIAAL_update_fooocus" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
-echo "python $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/launch.py \"@\"" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
+echo "AIIAAL_update_$aiiaalpkg" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
+echo "python $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/launch.py \"@\" &" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
+echo "sleep 60"
+echo "echo "Fooocus is still running in the background. Enter command: fooocus-exit to close down fooocus.""
 echo "" >> $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
 echo "Setting the new start file to be executable. (Authorization Required)"
 sudo chmod +x $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh
-echo "Creating executable link in /usr/bin --> AIIAAL-fooocus"
-sudo ln -sf "$pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh" "/usr/bin/AIIAAL-fooocus"
-echo "Installation complete. Start with command: $pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh"
+echo "Creating executable link in /usr/bin --> AIIAAL-$aiiaalpkg"
+sudo ln -sf "$pdirectory/AI-IntelArc-ArchLinux/$aiiaalpkg-IntelArc-ArchLinux/$aiiaalpkg-Start.sh" "/usr/bin/AIIAAL-$aiiaalpkg"
+echo "Installation complete. Start with command: AIIAAL-$aiiaalpkg with any fooocus arguments afterward"
