1a614ce50073db2136fa6b30dfd68d04b03a3c79
OCD Insomniac <119110518+JT-Gresham@users.noreply.github.com>
2024-05-30 15:13:45 -0400

    Update user_customize_fooocus.sh

--- fooocus-IntelArc-ArchLinux/user_customize_fooocus.sh
+++ fooocus-IntelArc-ArchLinux/user_customize_fooocus.sh
@@ -3,7 +3,10 @@ source AI-IntelArc-ArchLinux/libref
 ####  This file is for you to add any code you want to execute just before fooocus launches.  This will not be updated when AIIAAL or fooocus updates.
 ####  This is useful if you want to create custom presets and make sure your code, links, etc gets put back after an official update from the fooocus team.
 ####  I'll include the code I wrote to load my own custom preset as an example.  I didn't wnat to do this every time the fooocus team updated...
-echo $(pwd)
+
+####  THIS CODE STARTS IN YOUR PARENT DIRECTORY
+####    eg: where the AI-IntelArc-ArchLinux directory is located
+
 #### EXAMPLE CODE ####
 #  Change directory to make things easier
 cd $pdirectory/AI-IntelArc-ArchLinux
